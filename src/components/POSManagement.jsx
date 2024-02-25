d4b3f65b4dbdb77ccda2336b419e993945b5fc99
Brianna-Kline-SVSU <110587383+Brianna-Kline-SVSU@users.noreply.github.com>
2024-02-25 14:58:23 -0500

    Removed password field

--- src/components/POSManagement.jsx
+++ src/components/POSManagement.jsx
@@ -1,19 +1,8 @@
 import "../styles/PageStyles.css";
-import axios from "axios";
 import React, { useState } from "react";
 import SideBar from "./SideBar";
 import HorizotalNav from "./HorizontalNav";
-import OpenDayPage from "./OpenDay";
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useNavigate } from "react-router-dom";
-import routes from "../routes.js";
-import AddUser from "./AddUser.js";
-import EmployeeTable from "./EmployeeTable.js";
-import EditUser from "./EditUser.js";
 import AddPOS from "./AddPOS.jsx";
-import EditPOS from "./EditPos.jsx";
 import POSTable from "./POSTable.jsx";
 
 const POSManagementPage = () => {
