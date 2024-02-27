61b77b7cf4a6603cdc012913245215ce2f7d10e5
Brianna-Kline-SVSU <110587383+Brianna-Kline-SVSU@users.noreply.github.com>
2024-02-27 10:07:24 -0500

    AutoFocus on Login Screen and initial work on POS management screen

--- src/components/POSTable.jsx
+++ src/components/POSTable.jsx
@@ -1,38 +1,71 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 function POSTable() {
 
     //do a get request to get all the POS's for the current store
+    const [pos, setPosRegisters] = useState([]);
 
     const handleRowClick = (user) => {
         //setSelectedUser(user);
         //setIsEditFormOpen(true);
       };
 
-  const POS = [
-    {  City: 'Flint', State: 'MI', Postal_Code: '48507', district: 1,address:'3192 S. Linden Road' , name: 'POS 1 ', posID:'681' },
-    {  City: 'Flint', State: 'MI', Postal_Code: '48507', district: 1,address:'3192 S. Linden Road' , name: 'POS 2', posID:'682' },
-    // Add more POS objects here if needed
-  ];
+
+      
+  useEffect(() => {
+    function  fetchPosTable()    {
+      const url = `https://cis424-rest-api.azurewebsites.net/SVSU_CIS424/ViewRegistersByStoreID?storeID=0`;
+
+      axios.get(url)
+        .then((response) => {
+          console.log('Data:', response.data);
+
+          // Update the state variable 'pos' with the fetched data
+          setPosRegisters(response.data.map(pos => ({
+            ID: pos.ID,
+            name: pos.name,
+            storeID: pos.storeID,
+            enabled:pos.enabled,
+            opened:pos.opened
+            
+          })));
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }
+
+    // Call the function to initiate the GET request with specific details
+    fetchPosTable();
+  }, []); // Empty dependency array ensures that this effect runs only once, similar to componentDidMount
+
+
+
+
 
 
   return (
     <div>
-  <h2 className="text-lg font-bold mb-4">POS Registers at: -insert current store</h2>
       <table className="w-auto">
         <thead>
           <tr>
             <th className="px-4 py-2">POS Name</th>
-     
+            <th className="px-4 py-2">POS Current Status</th>
+            <th className="px-4 py-2"></th>
+
 
           </tr>
         </thead>
         <tbody>
-          {POS.map((pos) => (
-            <tr key={pos.name} onClick={() => handleRowClick(POS.name)} className="cursor-pointer hover:bg-gray-100">
-              <td className="border px-4 py-2">{pos.name}</td>
-            </tr>
-          ))}
+        {pos.map((pos) => (
+  <tr key={pos.name} onClick={() => handleRowClick(pos.name)} className="cursor-pointer hover:bg-gray-100">
+    <td className="border px-4 py-2">{pos.name}</td>
+    <td className="border px-4 py-2">{pos.opened ? 'Open' : 'Closed'}</td>
+    <td className="border px-4 py-2">{pos.enabled ? 'Enabled' : 'Disabled'}</td>
+  </tr>
+))}
+
         </tbody>
       </table>
         
