371cba5821d25a26a171bbf085c8731fb1a7e6f5
hwii23 <hjwright@svsu.edu>
2024-03-17 19:22:01 -0400

    Added Safe Expected count GET for Safe Audit page

--- src/components/SafeAudit.jsx
+++ src/components/SafeAudit.jsx
@@ -13,8 +13,6 @@ const SafeAuditPage = () => {
 	// Const for POST CreateCashCount request (https://cis424-rest-api.azurewebsites.net/SVSU_CIS424/CreateCashCount)
 	const CreateCashCountURL = "";
 
-	// GET Expected amount (https://cis424-rest-api.azurewebsites.net/SVSU_CIS424/GetOpenCount?storeID={storeID}&registerID={registerID})
-
 	// Const to hold the form data
     const [formData, setFormData] = useState({
         user: auth.cookie.user.ID,
@@ -51,24 +49,40 @@ const SafeAuditPage = () => {
 
 	// Loads expected count on page load
 	useEffect(() => {
-		// Get the expected amount from the server
+		function GetExpectedSafeCount() {
+			axios.get(
+				`https://cis424-rest-api.azurewebsites.net/SVSU_CIS424/ViewStoreObjects?storeID=${formData.store}`
+			)
+			.then((response) => {
+				// Get the safe ID from the response based on the name property
+				const safeID = response.data.find((obj) => obj.name === "SAFE").regID;
 
-		// Currently waiting for API
-		// axios
-		//     .get(
-		//         `https://cis424-rest-api.azurewebsites.net/SVSU_CIS424/GetOpenCount?storeID=${formData.store}&registerID=${formData.register}`
-		//     )
-		//     .then((response) => {
-		//         // Update the expected amount in the form data
-		//         setFormData((prevFormData) => ({
-		//             ...prevFormData,
-		//             expectedAmount: response.data,
-		//         }));
-		//     })
-		//     .catch((error) => {
-		//         console.log(error);
-		//     });
-		formData.expectedAmount = 0.00;
+				// If the safe ID is found, get the expected amount from the server
+				if (safeID != null) {
+					axios.get(
+						`https://cis424-rest-api.azurewebsites.net/SVSU_CIS424/GetOpenCount?storeID=${formData.store}&registerID=${safeID}`
+					)
+					.then((response) => {
+						// Update the expected amount in the form data
+						setFormData((prevFormData) => ({
+							...prevFormData,
+							expectedAmount: response.data,
+						}));
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+				}
+				else {
+					console.log("Safe not found");
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		}
+
+		GetExpectedSafeCount();
 	}, []);
 
 	// Function to check if amount fields are empty
