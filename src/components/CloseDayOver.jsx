7c70790126b8ba297b83c8cba131bbca9fb0b21f
David-Fegan <dlfegan96@gmail.com>
2024-04-05 17:20:00 -0400

    styling added to CloseDayCount

--- src/components/CloseDayOver.jsx
+++ src/components/CloseDayOver.jsx
@@ -9,12 +9,16 @@ const CloseDayOver = ({ onClose, details, isSafe }) => {
     return (
         <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-500 bg-opacity-50 flex justify-center items-center">
             <div className="bg-white p-8 rounded-lg shadow-lg">
-                <h2>Rule: Safe Limit Reached</h2>
+                <h2>Rule: Limit Reached</h2>
                 {isSafe ? (
                     <>
-                        <p>Safe has Reached the Max Amount.</p>
-                        <p>Please deposit the following to the bank:</p>
-                        <p></p>
+                        <div className="box-border text-center py-1 px-1 w-full border border-gray-300 hover:bg-white bg-white rounded-lg focus:outline-none focus:ring focus:border-blue-300">
+                            <p>Safe has Reached a Limit.</p>
+                        
+                            <p>Please deposit the following to the bank:</p>
+                        </div>
+                        <div className="box-border text-center py-1 px-1 w-full border border-gray-300 hover:bg-white bg-white rounded-lg focus:outline-none focus:ring focus:border-blue-300">
+
                         <ul>
                             <li>Hundreds: {details.hundred}</li>
                             <li>Fifties: {details.fifty}</li>
@@ -27,11 +31,13 @@ const CloseDayOver = ({ onClose, details, isSafe }) => {
                             <li>Nickel Rolls: {details.nickelRoll}</li>
                             <li>Penny Rolls: {details.pennyRoll}</li>
                         </ul>
+                        </div>
                     </>
                 ) : (
                     <>
-                        <p>Register has exceeded the threshold.</p>
+                        <p>Register has Reached a Limit.</p>
                         <p>Please move the following to the safe:</p>
+                        <p></p>
                         <ul>
                             <li>Hundreds: {details.hundred}</li>
                             <li>Fifties: {details.fifty}</li>
@@ -39,6 +45,7 @@ const CloseDayOver = ({ onClose, details, isSafe }) => {
                         </ul>
                     </>
                 )}
+                
                 <button onClick={onClose} className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Close
                 </button>
