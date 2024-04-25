5a4e61817185f6ecf4b4d93d5a2f82475d479420
plasmawario <mr.insainer@gmail.com>
2024-04-24 22:48:29 -0400

    fixed weird input on closeday input fields and decimal places behaving incorrectly

--- src/clamp.js
+++ src/clamp.js
@@ -8,6 +8,19 @@
  * @returns the clamped number
  */
 export function clamp(value, min = 0, max = 100000) {
+    //if the very last character is a ., append a 0 to make it number-acceptable and return it
+    if (value.toString()[value.toString().length - 1] === "."){
+      value = value.toString() + "0";
+
+      return value;
+    }
+
+    //if we try to type a number, let's say a 5, when the first decimal place is a 0 (like 12.0) then it will type it as 12.05
+    //this function will replace that .05 and turn it into a .5 so that it reads 12.5 as the user types it in
+    if (value.toString()[value.toString().length - 3] === "." && value.toString()[value.toString().length - 2] === "0" ){
+      value = value.toString().split(".")[0] + "." + value.toString().split(".")[1].substring(1);
+    }
+
     //first check if value is a number
     if (isNaN(Number(value))) {
       return Number(min);
